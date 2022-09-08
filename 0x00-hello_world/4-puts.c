445838a191756953f104a0d1d41003abf1ac2e13
Rawan Khalid <81408575+Rawan-Kh@users.noreply.github.com>
2022-09-08 20:25:36 +0200

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,5 @@
 #include <stdio.h>
 
-/**
- * main - Entry point
- *
- * Author: Sammykingx
- *
- * Return: Always 0 (Success)
- */
 int main(void)
 {
 	char string[70] = "\"Programming is like building a multilingual puzzle\"";
