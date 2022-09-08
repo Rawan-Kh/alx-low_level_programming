75d093dd01859957d33f87c73e12e26b8df4a5d5
Rawan Khalid <81408575+Rawan-Kh@users.noreply.github.com>
2022-09-08 20:36:03 +0200

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,9 @@
 
 int main(void)
 {
-	char string[70] = "\"Programming is like building a multilingual puzzle\"";
+// 	char string[70] = "\"Programming is like building a multilingual puzzle\"";
 	
-	puts(string);
+// 	puts(string);
 
 	/*  printing directly */
 
